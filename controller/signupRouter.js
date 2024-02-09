1d1b4a490d7ffe2a15ccdcb46a0449e99eed7259
Melvinpml <melvinpoulose06@gmail.com>
2024-02-09 12:16:11 +0530

    secirity signup done

--- controller/signupRouter.js
+++ controller/signupRouter.js
@@ -4,25 +4,27 @@ const bcrypt = require("bcryptjs")
 
 const router = express.Router()
 
-hashPassGen = async(pass)=>{
+hashPassGen = async (pass) => {
     const salt = await bcrypt.genSalt(10)
-    return bcrypt.hash(pass,salt)
+    return bcrypt.hash(pass, salt)
 }
 
-router.post("/add",async(req,res)=>{
-    let {data} = {"data":req.body}
+router.post("/signup", async (req, res) => {
+    let { data } = { "data": req.body }
     let password = data.password
     hashPassGen(password).then(
-        (hashPass)=>{
-            console.log(hashPass)
+        (hashPass) => {
+            
+            data.password = hashPass
+            let signup = new signupModel(data)
+            let result = signup.save()
+            res.json({ status: "success" })
+            console.log(data)
+
         }
     )
-
-    console.log(data)
-
-    // let signup = new signupModel(data)
-    // let result = await signup.save()
-    res.json({status:"success"})
 })
+
+
 
 module.exports = router
